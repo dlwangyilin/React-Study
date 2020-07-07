bbf02b5479b6b4c6c6e66265dc2050a9e0c74801
dlwangyilin <dlwangyilin@gmail.com>
2020-07-07 11:50:14 -0700

    Use component to render, but it would re-render page all the time.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -22,10 +22,9 @@ class Blog extends Component {
 
                 </header>
 
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" render={() => <h1>Home2</h1>} />
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
 
-                <Posts />
 
             </div>
         );
