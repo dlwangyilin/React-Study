e0892e8a3b9e891832b21b8bd04558803360049a
dlwangyilin <dlwangyilin@gmail.com>
2020-07-09 21:06:29 -0700

    Inital commits to learn Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const title = "Person Management";
-ReactDOM.render(<App appTitle={title}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/test.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({path: "../.env"});
-
-const foo = (props) => {
-    console.log(props);
-};
-
-foo("hello Woerd");
